87c38406e65d28732348c53ca26e130d93c75294
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-24 18:24:31 +0800

    注册input 验证及弹窗提示

--- js/register.js
+++ js/register.js
@@ -7,30 +7,61 @@ $(function() {
         codeVal = null,
         passwordVal = null;
     sendCodeBtn.click(function() {
-        if(!isSendClick){
-            //发送手机号，获取验证码
-
+        
+        phoneVal = $('#account').val()
+        if (!phoneVal) {
+            toastOpen('请输入手机号码')
+            return
+        }
+        if(!testPhone(phoneVal)) {
+            toastOpen('请输入有效的手机号码')
+            return
+        }
+        if (!isSendClick) {
             //倒计时
             countDown();
-        }else{
+            toastOpen('验证码已发送，请注意查收')
+            //发送手机号，获取验证码
+            
+        } else {
             //已点击
             return
-        }  
+        }
         isSendClick = true
+
     })
 
     regitsterBtn.click(function() {
         phoneVal = $('#account').val()
-        codeVal = $('#code')
-        passwordVal = $('#password')
+        codeVal = $('#code').val()
+        passwordVal = $('#password').val()
+        if(!phoneVal){
+            toastOpen('请输入手机号码')
+            return
+        }
+        if (!testPhone(phoneVal)) {
+            toastOpen('请输入有效的手机号码')
+            return
+        }
+        console.log(codeVal)
+        if(!codeVal){
+            toastOpen('请输入手机验证码')
+            return
+        }
+        if(!passwordVal) {
+            toastOpen('请输入登录密码')
+            return
+        }
         if(!isRegisterClick){
-            if(phoneVal && codeVal && passwordVal){
-                //提交数据...
-
-            }else{
-                //有为空的，tips提示
-                
-            }
+            //异步提交数据...
+            layer.open({
+                type:2,
+                content:'玩命登录中...'
+            })
+            //模拟异步登录3s
+            setTimeout(function() {
+                layer.closeAll()
+            },3000)
         }else{
             return
         }
@@ -57,5 +88,23 @@ $(function() {
             }
             default_count--
         },1000)
+    }
+    /**
+     * 校验手机号
+     * @param {string} phoneVal 
+     */
+    function testPhone(phoneVal) {
+        var patt = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+        return patt.test(phoneVal)
+    }
+    /**
+     * layer.open 封装
+     * @param {string} content 
+     */
+    function toastOpen(content) {
+        layer.open({
+            content:content,
+            time:2
+        })
     }
 })
