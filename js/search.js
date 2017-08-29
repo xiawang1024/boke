5efb24dbebc63bb0598323118468f63d697837be
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-08-29 11:24:14 +0800

    搜索页面

---
+++ js/search.js
@@ -0,0 +1,9 @@
+$(function(){
+    var schBtn = $('.g-search .sch-btn')
+    schBtn.click(function(){
+        var schKey = $('.g-search input').val()
+        console.log('------------------------------------');
+        console.log(schKey);
+        console.log('------------------------------------');
+    })
+})
